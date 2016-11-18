4362a4c8307697258251fc5b69d85df9d5783c3e
shanealv <shane.alv@gmail.com>
2016-11-17 21:30:18 -0700

    added test data and added code to deal with it

--- src/kmeans/main.cpp
+++ src/kmeans/main.cpp
@@ -4,21 +4,42 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <unistd.h>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	if (argc < 1)
+	if (argc <= 1)
 	{
 		cout << "Usage: ./kmeans [filename]" << endl;
 		return 0;
 	}
-	ifstream source(argv[1]);
+
+	char * filename = argv[argc - 1];
+	bool hasHeader = false;
+	int opt, k = 5;
+
+	while (getopt(argc, argv, "h") != -1) hasHeader = true;
+	while ((opt = getopt(argc, argv, "k")) != -1) k = atoi(argv[opt]);
+	cout << k << endl;
+	cin >> k;
+	if (k < 1)
+	{
+		cout << "invalid value for k";
+		return 0;
+	}
+
+	ifstream source(filename);
 	string line;
 	vector<vector<double>> data;
 	while (getline(source, line))
 	{
+		if (hasHeader)
+		{
+			hasHeader = false;
+			continue;
+		}
 		vector<double> record;
 		stringstream lineStream(line);
 		string cell;
@@ -30,10 +51,15 @@ int main(int argc, char* argv[])
 
 	int rows = data.size();
 
-	cout << "k-means Test Code" << endl;
-	auto output = kmeans::KMeans(data, 2);
+	cout << "Group | Data" << endl;
+	auto output = kmeans::KMeans(data, k);
 	for (int i = 0; i < rows; i++)
-		cout << data[i][0] << ", " << data[i][1] << "\t" << output[i] << endl;
+	{
+		cout << output[i] << " | ";
+		for (auto feature : data[i])
+			cout << feature << " ";
+		cout << endl;
+	}
 	cout << endl;
 	return 0;
 }
